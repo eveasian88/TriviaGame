bbe53d46869e5e984a9b870f36b7c3b2ddf4673a
Susye Weng-Reeder <weng-reeder@hotmail.com>
2018-10-14 12:34:50 -0700

    questions brainstorm

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+// create ten questions to start game
+$(document).ready(function () {
+    var options = [
+        {
+            question: "The majority of leagues follow whose organizations rules and safety standards?",
+            choice: ["CLTA", "WTFDA", "AATSP", "RRG"],
+            answer: 2,
+            photo: "assets/images/wftda.jpg"
+        }];
