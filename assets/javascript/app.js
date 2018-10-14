42ec0fe7e882e1fd759f4d0bae6e729e2bcbb596
Susye Weng-Reeder <weng-reeder@hotmail.com>
2018-10-14 16:29:51 -0700

    timer trials

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -114,30 +114,87 @@ $(document).ready(function () {
         running = false;
         clearInterval(intervalId);
     }
-    
+
     // randomly pick question in array if not already shown
     // display question and loop though and display possible answers
     function displayQuestion() {
         // generate random index in array
-        index = Math.floor(Math.random()*options.length);
+        index = Math.floor(Math.random() * options.length);
         pick = options[index];
-    
-    //	if (pick.shown) {
-    //		//recursive to continue to generate new index until one is chosen that has not shown in this game yet
-    //		displayQuestion();
-    //	} else {
-    //		console.log(pick.question);
-            // iterate through answer array and display
-            $("#questionblock").html("<h2>" + pick.question + "</h2>");
-            for(var i = 0; i < pick.choice.length; i++) {
-                var userChoice = $("<div>");
-                userChoice.addClass("answerchoice");
-                userChoice.html(pick.choice[i]);
-                // assign array position to it so it can check answer
-                userChoice.attr("data-guessvalue", i);
-                $("#answerblock").append(userChoice);
-    //		}
-    }
-    
-    //click function to select answer and outcomes
 
+        //	if (pick.shown) {
+        //		//recursive to continue to generate new index until one is chosen that has not shown in this game yet
+        //		displayQuestion();
+        //	} else {
+        //		console.log(pick.question);
+        // iterate through answer array and display
+        $("#questionblock").html("<h2>" + pick.question + "</h2>");
+        for (var i = 0; i < pick.choice.length; i++) {
+            var userChoice = $("<div>");
+            userChoice.addClass("answerchoice");
+            userChoice.html(pick.choice[i]);
+            // assign array position to it so it can check answer
+            userChoice.attr("data-guessvalue", i);
+            $("#answerblock").append(userChoice);
+            //		}
+        }
+
+        //click function to select answer and outcomes
+        $(".answerchoice").on("click", function () {
+            //grab array position from userGuess
+            userGuess = parseInt($(this).attr("data-guessvalue"));
+        
+            //correct guess or wrong guess outcomes
+            if (userGuess === pick.answer) {
+                stop();
+                correctCount++;
+                userGuess="";
+                $("#answerblock").html("<p>Correct!</p>");
+                hidepicture();
+        
+            } else {
+                stop();
+                wrongCount++;
+                userGuess="";
+                $("#answerblock").html("<p>Wrong! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+                hidepicture();
+            }
+        })
+        }
+        
+
+
+        //run the score screen if all questions answered
+        if ((wrongCount + correctCount + unanswerCount) === qCount) {
+            $("#questionblock").empty();
+            $("#questionblock").html("<h3>Game Over!  Here's how you did: </h3>");
+            $("#answerblock").append("<h4> Correct: " + correctCount + "</h4>");
+            $("#answerblock").append("<h4> Incorrect: " + wrongCount + "</h4>");
+            $("#answerblock").append("<h4> Unanswered: " + unanswerCount + "</h4>");
+            $("#reset").show();
+            correctCount = 0;
+            wrongCount = 0;
+            unanswerCount = 0;
+
+        } else {
+            runTimer();
+            displayQuestion();
+
+        }
+    }, 3000);
+
+}
+
+$("#reset").on("click", function () {
+    $("#reset").hide();
+    $("#answerblock").empty();
+    $("#questionblock").empty();
+    for (var i = 0; i < holder.length; i++) {
+        options.push(holder[i]);
+    }
+    runTimer();
+    displayQuestion();
+
+})
+
+    })
